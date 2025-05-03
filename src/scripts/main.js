42d0e89c3c5c0331c151965b37b3567c6194ec12
Yulia Fil <filyulia.yf@gmail.com>
2025-05-03 14:55:53 +0300

    solution with height

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,11 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 const sWidth = spider.offsetWidth;
-const sHeigth = spider.offsetWidth;
+const sHeight = spider.offsetHeight;
 const wWidth = wall.offsetWidth;
-const wHeigth = wall.offsetWidth;
+const wHeight = wall.offsetHeight;
 const x = (wWidth - sWidth) / 2;
-const y = (wHeigth - sHeigth) / 2;
+const y = (wHeight - sHeight) / 2;
 
 spider.style.left = x + 'px';
 spider.style.top = y + 'px';
