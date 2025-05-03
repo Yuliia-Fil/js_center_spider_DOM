ce4f5a18dafcf71779d05ca54184ab2f57c6ad00
Yulia Fil <filyulia.yf@gmail.com>
2025-05-03 14:56:15 +0300

    make build

---
+++ index.2de6434f.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),f=t.offsetWidth,o=t.offsetHeight,l=e.offsetWidth,s=e.offsetHeight;t.style.left=(l-f)/2+"px",t.style.top=(s-o)/2+"px";
+//# sourceMappingURL=index.2de6434f.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const sWidth = spider.offsetWidth;
+const sHeight = spider.offsetHeight;
+const wWidth = wall.offsetWidth;
+const wHeight = wall.offsetHeight;
+const x = (wWidth - sWidth) / 2;
+const y = (wHeight - sHeight) / 2;
+spider.style.left = x + "px";
+spider.style.top = y + "px";
+
+//# sourceMappingURL=index.f75de5e1.js.map
